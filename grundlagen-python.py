a0e1a5fab1aef4fd466ba010ca871a2521107a15
Aaron136 <171wita28@o365.htl-leoben.at>
2018-04-23 14:37:21 +0200

    python datei angelegt

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -16,3 +16,11 @@ wer = input("Und wer bist du? ")
 # und gibt den Text wieder aus
 print("Hallo", wer)
 
+if(wer == "ich"):
+    print ("Hallo Du!")
+else:
+    print ("Hallo ", wer)
+
+lieblingszahl = input("Was ist deine Lieblingszahl?")
+print("Super, ich mag die Zahl ", lieblingszahl)
+print("Aber die groessere Zahl", int (lieblingszahl)+10, "mag ich noch mehr!")
