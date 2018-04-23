e5b2745f89816186362090c98950cc4ced883f32
Aaron136 <171wita28@o365.htl-leoben.at>
2018-04-23 16:24:51 +0200

    python datei angelegt

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,4 +1,5 @@
 # zahlenraten.py
+import random
 max = 200
 zufallszahl = random.randint(1,200)
 gewonnen = False
@@ -8,14 +9,15 @@ print("Bitte gib eine zahl zwischen 1", max, "eingeben")
 while(gewonnen == False):
     zahl = input("Welche Zahl?")
     zahl = int(zahl)
-if (zahl == zufallszahl):
-    gewonnen = True
+    if (zahl == zufallszahl):
+        gewonnen = True
 
-if(zahl < zufallszahl):
-    print("Die zahl ist größer")
-    versuche+1
-    zahl = input("Welche Zahl?")
-if(zahl > zufallszahl):
-    print("Die Zahl ist kleiner")
-    versuche+1
-    zahl = input("Welche Zahl?")
+    if(zahl < zufallszahl):
+        print("Die zahl ist größer")
+        versuche+1
+        zahl = input("Welche Zahl?")
+        
+    if(zahl > zufallszahl):
+        print("Die Zahl ist kleiner")
+        versuche+1
+        zahl = input("Welche Zahl?")
