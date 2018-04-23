8ed189892d6c2d1cfa04cc75b76a2cc1b7639135
Aaron136 <171wita28@o365.htl-leoben.at>
2018-04-23 16:17:54 +0200

    python datei angelegt

---
+++ zahlenraten.py
@@ -0,0 +1,21 @@
+# zahlenraten.py
+max = 200
+zufallszahl = random.random(1,200)
+gewonnen = False
+versuche = 0
+print("Bitte gib eine zahl zwischen 1", max, "eingeben")
+
+while(gewonnen == False):
+    zahl = input("Welche Zahl?")
+    zahl = int(zahl)
+if (zahl == zufallszahl):
+    gewonnen = True
+
+if(zahl < zufallszahl):
+    print("Die zahl ist größer")
+    versuche+1
+    zahl = input("Welche Zahl?")
+if(zahl > zufallszahl):
+    print("Die Zahl ist kleiner")
+    versuche+1
+    zahl = input("Welche Zahl?")
