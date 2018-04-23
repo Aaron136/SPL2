9321745d4f5059ab433f0f95ae4887bf1b9f9aea
Aaron136 <171wita28@o365.htl-leoben.at>
2018-04-23 14:08:55 +0200

    python datei angelegt

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -1,1 +1,18 @@
 # grundlagen-python.py
+
+# Kommentare erfolgen mit hashtag
+
+# Ausgabe von Daten 
+print("Hello World")
+
+# Variable definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
+
+print(heimat, "an World: ", "Hallo")
+
+# Eingabe
+wer = input("Und wer bist du? ")
+
+# und gibt den Text wieder aus
+print("Hallo", wer)
+
